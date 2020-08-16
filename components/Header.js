7d5aa6e26f0fae437eda8f09e82f24b2d52ff447
matieuruth <matieu.ruth@gmail.com>
2020-08-16 19:28:54 -0400

    compleated header

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,32 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+
+function Header() {
+
+    //elements
+    const headerTitle = document.createElement('div');
+    const spanDate = document.createElement('span');
+    const h1 = document.createElement('h1');
+    const spanTemp = document.createElement('spam');
+
+    //classes
+    headerTitle.classList.add('header');
+    spanDate.classList.add('date');
+    spanTemp.classList.add('temp');
+
+    //structure
+    headerTitle.appendChild(spanDate);
+    headerTitle.appendChild(h1);
+    headerTitle.appendChild(spanDate);
+
+    //content
+    spanDate.textContent = 'MARCH 28, 2020';
+    h1.textContent = 'Lambda Times';
+    spanTemp.textContent = '98°';
+
+    return headerTitle;
+}
+
+headerContainer.appendChild(Header());
